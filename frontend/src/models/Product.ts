9c2ee88d21eb81bc233c1c8226a355bd61b8c6fd
CSteentoft <143799991+CSteentoft@users.noreply.github.com>
2026-03-13 16:34:54 +0100

    Exercise 1.3-1.4  -  abstracted products to extend on Product.ts eliminating duplicate logic, introduced getPriceWithoutTaxes by adding it to Product.ts for all products to use.

---
+++ frontend/src/models/Product.ts
@@ -0,0 +1,26 @@
+import { ProductType } from "./ProductType";
+
+export abstract class Product {
+    public title: string;
+    public imageUrl: string;
+    public basePrice: number;
+
+    public taxRate: number = 1.25;
+    public discountRate: number = 0.10;
+
+    public abstract productType: ProductType;
+
+    constructor(title: string, imageUrl: string, basePrice: number) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.basePrice = basePrice;
+    }
+
+    public getPrice(): number {
+        return Math.round((this.basePrice * (1 - this.discountRate)) * this.taxRate);
+    }
+
+    public getPriceWithoutTaxes(): number {
+        return Math.round(this.basePrice * (1 - this.discountRate));
+    }
+}
